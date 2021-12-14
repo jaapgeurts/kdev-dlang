aa9be3a6886296fab7ac3eef904dc504e376ffc5
Jaap Geurts <jaap.geurts@fontys.nl>
2021-12-14 13:34:50 +0100

    Added missing debug files

---
+++ dlang/parser/debug.cpp
@@ -0,0 +1,7 @@
+// <one line to give the program's name and a brief idea of what it does.>
+// SPDX-FileCopyrightText: 2021 Jaap Geurts <jaapg@gmx.net>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "debug.h"
+
+Q_LOGGING_CATEGORY(DPARSER, "kdev.dlang.parser")
---
+++ dlang/parser/debug.h
@@ -0,0 +1,9 @@
+// <one line to give the program's name and a brief idea of what it does.>
+// SPDX-FileCopyrightText: 2021 Jaap Geurts <jaapg@gmx.net>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(DPARSER)
